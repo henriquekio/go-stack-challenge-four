e7f0f11882f69e9e4bce6344bdbd0586625e8511
Henrique Pereira <wafx.henrique@grupofleury.com.br>
2020-09-29 16:49:41 -0300

    Finalizando desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -14,15 +15,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc: number, cur: Transaction) => (acc += cur.value), 0);
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc: number, cur: Transaction) => (acc += cur.value), 0);
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = {
+      id: uuid(),
+      title,
+      type,
+      value,
+    };
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
